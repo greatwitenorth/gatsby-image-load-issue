0fe32de43d8dc99fce29edb0b7dd7fb9c2689696
Nick Verwymeren <nick@teamgi.ca>
2019-09-17 09:19:50 -0500

    added images

--- src/components/image.js
+++ src/components/image.js
@@ -16,17 +16,21 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+      placeholderImages: allFile(filter: {extension: {eq: "jpeg"}}) {
+        edges{
+          node{
+            childImageSharp{
+              fluid(maxWidth: 300){
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return data.placeholderImages.edges.map(image => <Img fluid={image.node.childImageSharp.fluid} />)
 }
 
 export default Image
